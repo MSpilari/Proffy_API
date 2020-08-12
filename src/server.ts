1ef57b1941c585dc1f1eb5e7d2a8f9b822177004
MSpilari <mb.spilari@hotmail.com>
2020-08-12 16:12:58 -0300

    Heroku server commit

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import express from "express";
 import cors from "cors";
+import dotenv from 'dotenv';
 
 import routes from "./routes";
 
+dotenv.config()
+
 const app = express();
+
+const port = process.env.PORT_URL || 3333
 
 app.use(express.json());
 app.use(cors());
 
 app.use(routes);
 
-app.listen(3333);
+app.listen(port);
