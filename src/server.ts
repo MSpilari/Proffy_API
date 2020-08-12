753e0731dc9be412ff858ff9293243c242be7d9d
MSpilari <mb.spilari@hotmail.com>
2020-08-12 16:34:06 -0300

    Heroku server commit

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
 import express from "express";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import routes from "./routes";
 
+dotenv.config();
 
 const app = express();
 
-const port = process.env.PORT || 3333
+const port = process.env.PORT || 3333;
 
-app.set('port', port)
+app.set("port", port);
 
 app.use(express.json());
 app.use(cors());
 
 app.use(routes);
 
-app.listen(port);
+app.listen(port, () => console.log("App is running on port " + port));
